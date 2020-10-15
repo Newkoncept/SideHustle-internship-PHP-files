6d2c840129b0b640ea0c243ad67542b46985c404
Taiwo Oluwagbemiga <49463561+Newkoncept@users.noreply.github.com>
2020-10-15 06:31:56 +0100

    First PHP major task

---
+++ basicSyntax-MajorTask.php
@@ -0,0 +1,25 @@
+<?php
+
+    //******** First major task. ********
+
+    /*
+        Occupation: Student
+        Gender: Male
+        Sidehustle Username: Oluwagbemiga
+    */
+
+    //******** About Me ********
+    echo "<h1> About Me<h1/> <hr/>";
+    
+    //******** Slack Username ********
+    echo "<h3> Oluwagbemiga</h3>";
+
+    //******** About Myself ********
+    echo "<p> My name is Taiwo Oluwagbemiga, a graduate of Obafemi Awolowo University where 
+            I backed my degree in Political Science. 
+        </p>";
+    echo "<p> I am a tech lover, a lover of God and also I love to explore things especially 
+            in the tech line with an efficient skill in Frontend Website Development and Wordpress. 
+        </p>";
+    echo "<p> I am a Website Development and UI/UX intern at SideHustle.<p/>";
+?>
